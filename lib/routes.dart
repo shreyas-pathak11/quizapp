d473a4dd25e1d6247ce4c7d792f8e0d81d551074
shreyas-pathak11 <shreyaspathak@theaware.ai>
2025-01-12 22:58:45 +0530

    first commit

---
+++ lib/routes.dart
@@ -0,0 +1,13 @@
+import 'package:quizapp/about/about.dart';
+import 'package:quizapp/home/home.dart';
+import 'package:quizapp/login/login.dart';
+import 'package:quizapp/profile/profile.dart';
+import 'package:quizapp/topics/topics.dart';
+
+var appRoutes = {
+  '/': (context) => const Home(),
+  '/login': (context) => const Login(),
+  '/about': (context) => const About(),
+  '/profile': (context) => const Profile(),
+  '/topics': (context) => const Topics(),
+};
